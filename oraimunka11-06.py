b8aaf0ae569c8b576739d9e04db8d07395c5f03f
NiedermayerDavid <145555492+NiedermayerDavid@users.noreply.github.com>
2023-11-06 14:15:08 +0100

    Update oraimunka11-06.py

--- oraimunka11-06.py
+++ oraimunka11-06.py
@@ -1,15 +1,16 @@
 ev = (int(input("adjon meg egy évszámot: ")))
 
+if ev < 1582:
+    print("Nem a gergely-korszakba esik")
+else:
+    print("A gergely-korszakba esik")
+
 print(ev % 4)
 if ev % 4 != 0:
     print("normál év van.")
-
 elif ev % 400 != 0:
     print("normál év van")
 elif ev % 100 != 0:
     print("Szökő év van")
 else:
     print("szökő év van")
-
-if ev < 1582:
-    print("Nem a gergely-korszakba esik")
